eecf4f56d64d863bbdece8754b2e344b78ae1a0c
Eric Bishard <e.bishard@gmail.com>
2019-01-20 19:58:16 -0800

    Add RepoGrid and check for null after fetch before rendering grid.
    Add Styles for RepoGrid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,66 @@
 import React from 'react';
 import api from '../utils/api';
+import PropTypes from 'prop-types';
+
+function SelectLanguage(props) {
+  var languages = ['All', 'JavaScript', 'C#', 'Java', 'Python', 'CSS'];
+
+  return (
+    <div className='container'>
+      <div className='container-item'>
+        <ul className='languages'>
+          {languages.map(function (lang) {
+            return (
+              <li
+                style={
+                  lang === props.selectedLanguage
+                    ? { color: 'rgb(151, 243, 255, 0.5)' }
+                    : null
+                }
+                onClick={props.onSelect.bind(null, lang)}
+                key={lang}
+              >
+                {lang}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
+
+function RepoGrid(props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img
+                  src={repo.owner.avatar_url}
+                  className='avatar'
+                  alt={'avatar for ' + repo.owner.login} />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+RepoGrid.propTypes = {
+  repo: PropTypes.array.isRequired
+}
 
 class Popular extends React.Component {
   constructor(props) {
@@ -12,6 +73,7 @@ class Popular extends React.Component {
   }
 
   componentDidMount() {
+    // AJAX SHIT HERE
     this.updateLanguage(this.state.selectedLanguage);
   }
 
@@ -24,53 +86,26 @@ class Popular extends React.Component {
     });
     api.fetchPopularRepos(lang)
       .then((repos) => {
-        console.log(repos)
+        this.setState(function() {
+          return {
+            repos: repos
+          }
+        }.bind(this))
       });
   }
   render() {
     return (
-      <>
+      <div>
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-      </>
+        {!this.state.repos
+          ? <p>loading..</p>
+          : <RepoGrid repos={this.state.repos} />}
+      </div>
     )
   }
 }
 
 export default Popular;
-
-/* SelectLanguage Component */
-import PropTypes from 'prop-types';
-
-function SelectLanguage(props) {
-  var languages = ["All", "JavaScript", "C#", "Java", "Python", "CSS"];
-  return (
-    <div className="container">
-      <div className="container-item">
-        <ul className="languages">
-          {languages.map(function (lang) {
-            return (
-              <li
-                style={
-                  lang === props.selectedLanguage
-                    ? { color: 'rgb(151, 243, 255, 0.5)' }
-                    : null
-                }
-                onClick={props.onSelect.bind(null, lang)}
-                key={lang}>
-                {lang}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-SelectLanguage.propTypes = {
-  selectedLanguage: PropTypes.string.isRequired,
-  onSelect: PropTypes.func.isRequired
-}
