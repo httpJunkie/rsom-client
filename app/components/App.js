f8cfe9091a7ba647941931d032fa919c686b53d5
Eric Bishard <e.bishard@gmail.com>
2019-02-18 08:40:20 -0800

    Add React Router, Links, NavLinks and 404, Battle & Home pages
    Also setup webpack.config to treat  routes specially using:
    
    devServer, historyAPiFallback equals true and publicPath set to '/'

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Popular from './Popular';
 import Iconic from './Iconic';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Popular />
-        <Iconic />
-      </>
+      <BrowserRouter>
+        <div className="popular-container">
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/battle" component={Battle} />
+          <Route path="/popular" render={(props) =>
+            <div>
+              <Popular />
+              <Iconic />
+            </div>
+          } />
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Battle() {
+  return (
+    <div className="home-container">
+      <h1>Battle Coming Soon.</h1>
+    </div>
+  )
+}
+
+export default Battle;
