e7ba6d353600d415b719dbfb49b45c12f47860f0
Eric Bishard <e.bishard@gmail.com>
2019-02-18 08:40:31 -0800

    oops

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Popular from './Popular';
 import Iconic from './Iconic';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Nav from './Nav';
 import Home from './Home';
 import Battle from './Battle';
@@ -12,14 +12,19 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="popular-container">
           <Nav />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/battle" component={Battle} />
-          <Route path="/popular" render={(props) =>
-            <div>
-              <Popular />
-              <Iconic />
-            </div>
-          } />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" render={(props) =>
+              <div>
+                <Popular />
+                <Iconic />
+              </div>
+            } />
+            <Route render={() => {
+              return <h2 className="four-o-four">404 Page Not Found</h2>
+            }}/>
+          </Switch>
         </div>
       </BrowserRouter>
     )
